0b5bcc1f1f5dae432250f9622b268391f99ba706
Vadim Makishvili <makishvili@yandex-team.ru>
2014-03-26 01:25:38 +0200

    Форматирование кода

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,25 +2,21 @@
 
 REPO_URL=`git config --get remote.origin.url`
 
-if [ ! -d static ];
-    then
-        mkdir static
-        cd static
-
-        if git fetch && git branch -a | grep gh-pages;
-            then
-                git clone -b gh-pages "$REPO_URL" .
-            else
-                git init
-                git remote add origin "$REPO_URL"
-                git checkout -b gh-pages
-                touch README.md
-                git add -A
-                git commit -m "init"
-                git push origin gh-pages:gh-pages
-        fi
-
-        cd ../
+if [ ! -d static ]; then
+    mkdir static
+    cd static
+    if git branch -a | grep gh-pages; then
+        git clone -b gh-pages "$REPO_URL" .
+    else
+        git init
+        git remote add origin "$REPO_URL"
+        git checkout -b gh-pages
+        touch README.md
+        git add -A
+        git commit -m "init"
+        git push origin gh-pages:gh-pages
+    fi
+    cd ../
 fi
 
 YENV=production node_modules/.bin/enb make
